ed47a47b35f6d828528e8c3313fb2f4b6b925def
ViaticusRex <vdelacruz417@gmail.com>
2024-08-14 13:15:27 -0400

    I was able to fetch all of my data and render it in my Animes component.

--- frontend/src/Components/Animes.js
+++ frontend/src/Components/Animes.js
@@ -1,11 +1,33 @@
 import Anime from "./Anime";
+import { useEffect, useState } from "react";
 
 function Animes() {
   //fetch all animes from the backend and render them as a list using the Anime component. Make sure to style the animes to look like the screenshot from the README. Feel free to use axios to grab data
 
+  const API = process.env.REACT_APP_API;
+
+  const [animes, setAnimes] = useState([]);
+
+  //call to API to get all animes
+  useEffect(() => {
+    fetch(`${API}/animes`)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+        setAnimes(res)
+      })
+      .catch(error => console.log(error))
+  }, []);
+
+
   return (
     <section className="index" id="anime-list">
-      <p>No animes to display</p>
+      <div>
+        {animes.map(anime => (
+          <Anime key={anime.id} anime={anime} />
+        ))}
+      </div>
+
     </section>
   );
 }
